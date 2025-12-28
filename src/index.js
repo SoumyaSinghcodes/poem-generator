7f224c32a18d44e7deb4b20e60bc0cfa926366a5
Soumya Singh <soumyasinghthakur147@gmail.com>
2025-12-28 15:02:06 +0530

    changes in ndex.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ function generatePoem(event) {
 
   let instructionInput = document.querySelector("#user-instruction");
 
-  let apiKey = "1cad20tb75afd483of055fc5b84c9599";
+  
   let prompt = `User instructions: Generate an English poem about ${instructionInput.value}`;
   let context =
     "You are an expert and love to write. Your mission is to generate a 4 line of  poems, jokes, recipes, quotes, travel destinations, baby names, etc in basic HTML. Make aure to follow the user instructions.Do not include a title to the content. Sign the poem by 'Soumya Singh's AI Generator' inside a <strong> element at the end of the content";
